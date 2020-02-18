6206d45ebc4621bf20e8171cdd5fe40389757385
Prakhar <33597861+0LUKE1@users.noreply.github.com>
2020-02-18 06:23:07 +0530

    Add files via upload

---
+++ sort_string_pointer.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    char *T;
+    int I,J,K;
+    char *ARRAY[5]={"SUNIL","ANIL","DILIP","JAY","BHARAT"};
+    clrscr();
+    for(I=0;I<5;I++)
+    {
+       printf("%s \t",ARRAY[I]);
+    }
+    printf("\n");
+    for(I=0;I<4;I++)
+    {
+      for(J=0;J<4-I;J++)
+      {
+ K=strcmp(ARRAY[J],ARRAY[J+1]);
+ if(K>0)
+ {
+   T=ARRAY[J];
+   ARRAY[J]=ARRAY[J+1];
+   ARRAY[J+1]=T;
+ }
+      }
+    }
+    for(I=0;I<5;I++)
+    {
+       printf("%s \t",ARRAY[I]);
+    }
+getch();
+}
